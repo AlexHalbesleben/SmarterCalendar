c377edc1a38adf0db43afcb52ebf03b5a79b5428
AlexHalbesleben <alexhalb@outlook.com>
2022-07-23 00:31:45 -0500

    Deploy script for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/SmarterCalendar/"
+  publicPath: process.env.NODE_ENV === "production" ? "/SmarterCalendar" : "/",
 });
