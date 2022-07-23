bc4e1dbc78b8691ab1920032ba57d020312f3a24
AlexHalbesleben <alexhalb@outlook.com>
2022-07-23 01:02:51 -0500

    Fix typo in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { store } from "./store";
+import { store } from "./store/index.vuex";
 
 import { BootstrapVue } from "bootstrap-vue";
 
