8d24280cf4d7e2b8e8396f479995dee413238e91
AlexHalbesleben <alexhalb@outlook.com>
2022-07-23 23:52:39 -0500

    Create basic settings modal

---
+++ src/components/SettingsModal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="settingsmodal-container">
+    <b-modal id="settings-modal" title="Settings">
+      <div class="container"></div>
+    </b-modal>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import vxm from "../store/index.vuex";
+
+@Component
+export default class SettingsModal extends Vue {}
+</script>
+<style lang="scss" scoped></style>
