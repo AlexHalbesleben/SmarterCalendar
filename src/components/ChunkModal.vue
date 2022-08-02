4ad1e27a30f22d0b8dcb1f6e49e9d01b57b5c4a4
AlexHalbesleben <alexhalb@outlook.com>
2022-08-01 20:54:46 -0500

    Base chunk modal

---
+++ src/components/ChunkModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chunkmodal-container">
+    <b-modal id="chunk-modal" :title="chunk?.task.name"> </b-modal>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import vxm from "@/store/index.vuex";
+import Chunk from "@/types/Chunk";
+
+@Component
+export default class ChunkModal extends Vue {
+  get chunk(): Chunk | undefined {
+    return vxm.store.editedChunk;
+  }
+}
+</script>
+<style lang="scss" scoped></style>
