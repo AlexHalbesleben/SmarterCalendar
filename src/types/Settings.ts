a1a183e0f35569e849a3a0bf38901559b8efcf9c
AlexHalbesleben <alexhalb@outlook.com>
2022-08-03 16:11:27 -0500

    Enable settings to process daily times

--- src/types/Settings.ts
+++ src/types/Settings.ts
@@ -6,6 +6,36 @@ export default class Settings {
 
   dailyStartTimes: Record<number, string> = {};
   dailyEndTimes: Record<number, string> = {};
+
+  get dailyTimes(): number[] {
+    const ret: number[] = [];
+
+    for (let day = 0; day < 7; day++) {
+      let startTime = this.baseStartTime;
+      if (this.dailyStartTimes[day]) {
+        const strStartTime = this.dailyStartTimes[day];
+        const extracted = strStartTime.match(/([0-9]{2}):([0-9]{2})/);
+
+        if (extracted && extracted[1] && extracted[2]) {
+          startTime = parseInt(extracted[1]) * 60 + parseInt(extracted[2]);
+        }
+      }
+
+      let endTime = this.baseEndTime;
+      if (this.dailyEndTimes[day]) {
+        const strEndTime = this.dailyEndTimes[day];
+        const extracted = strEndTime.match(/([0-9]{2}):([0-9]{2})/);
+
+        if (extracted && extracted[1] && extracted[2]) {
+          endTime = parseInt(extracted[1]) * 60 + parseInt(extracted[2]);
+        }
+      }
+
+      ret.push(endTime - startTime);
+    }
+
+    return ret;
+  }
 }
 
 export const SETTINGS_DESCRIPTIONS = {
