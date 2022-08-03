d1b1021961728040e81abe6b22bc1ffad93ea7df
AlexHalbesleben <alexhalb@outlook.com>
2022-08-03 14:38:43 -0500

    Add basic weekday capabilities

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -39,7 +39,7 @@
             />
           </b-input-group>
         </div>
-        <div class="row">
+        <div class="row mb-2">
           <b-input-group class="col" prepend="End time">
             <b-form-timepicker
               @input="updateEnd"
@@ -47,6 +47,38 @@
               @blur="vxm.store.updateChunks()"
             />
           </b-input-group>
+        </div>
+
+        <div class="mt-2">
+          <div v-for="(j, i) in 7" :key="i" class="mb-2">
+            <b-input-group :prepend="weekday(i)">
+              <div class="flex-grow-1">
+                <b-input-group class="row no-gutters">
+                  <b-input-group-prepend is-text class="settings-day-prepend">
+                    Start time
+                  </b-input-group-prepend>
+                  <b-form-timepicker />
+                  <b-input-group-append is-text class="settings-day-append-top">
+                    <b-form-checkbox class="mr-n2" />
+                  </b-input-group-append>
+                </b-input-group>
+                <b-input-group
+                  class="row no-gutters settings-modal-bottom-day-row"
+                >
+                  <b-input-group-prepend is-text class="settings-day-prepend">
+                    End time
+                  </b-input-group-prepend>
+                  <b-form-timepicker />
+                  <b-input-group-append
+                    is-text
+                    class="settings-day-append-bottom"
+                  >
+                    <b-form-checkbox class="mr-n2" />
+                  </b-input-group-append>
+                </b-input-group>
+              </div>
+            </b-input-group>
+          </div>
         </div>
       </div>
     </b-modal>
@@ -56,6 +88,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import vxm from "../store/index.vuex";
 import { SETTINGS_DESCRIPTIONS } from "@/types/Settings";
+import { WeekDays } from "@/types/Calendar";
 
 @Component
 export default class SettingsModal extends Vue {
@@ -115,6 +148,39 @@ export default class SettingsModal extends Vue {
     const end = vxm.store.settings.baseEndTime;
     return `${Math.floor(end / 60)}:${Math.floor(end % 60)}`;
   }
+
+  weekday(num: number): string {
+    return WeekDays[num];
+  }
+
+  dayStartLocked(day: number): boolean {
+    return Object.keys(vxm.store.settings.dailyStartTimes).includes(
+      day.toString()
+    );
+  }
+
+  dayEndLocked(day: number): boolean {
+    return Object.keys(vxm.store.settings.dailyStartTimes).includes(
+      day.toString()
+    );
+  }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.settings-day-prepend > .input-group-text {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.settings-modal-bottom-day-row {
+  margin-top: -1px; // Removes "double border"
+}
+
+.settings-day-append-top > .input-group-text {
+  border-bottom-right-radius: 0;
+}
+
+.settings-day-append-bottom > .input-group-text {
+  border-top-right-radius: 0;
+}
+</style>
