84ffeed533c943046d02c583c2ab18d6bac107b3
AlexHalbesleben <alexhalb@outlook.com>
2022-08-04 00:40:19 -0500

    Basic help modal

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,21 @@
 <template>
   <footer class="w-100 bg-primary fixed-bottom p-2">
-    By
-    <b-link href="https://github.com/AlexHalbesleben" class="text-secondary"
-      >Alex Halbesleben</b-link
+    <b-link
+      href="https://github.com/AlexHalbesleben/SmarterCalendar"
+      class="text-secondary"
     >
+      Code
+    </b-link>
+    by
+    <b-link href="https://github.com/AlexHalbesleben" class="text-secondary">
+      Alex Halbesleben
+    </b-link>
+
+    <div class="float-right">
+      <b-button variant="info" class="text-dark" v-b-modal.how-to-use-modal>
+        ?
+      </b-button>
+    </div>
   </footer>
 </template>
 <script lang="ts">
