be7bebc2d51a9ea90d16eef47292474196c9f8f8
AlexHalbesleben <alexhalb@outlook.com>
2022-08-30 21:13:04 -0500

    Bump version from 3.2.7 to 3.2.8

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <b-navbar variant="primary" fixed="top">
       <b-navbar-brand>
         SmarterCalendar
-        <span class="text-secondary">v3.2.7</span>
+        <span class="text-secondary">v3.2.8</span>
       </b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-button class="text-dark" variant="info" v-b-modal.settings-modal>
