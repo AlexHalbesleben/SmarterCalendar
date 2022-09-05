4ae439b36e7b909823e21b6b21db745d800088ec
AlexHalbesleben <alexhalb@outlook.com>
2022-09-05 01:28:19 -0500

    alright it didn't work

--- src/store/index.vuex.ts
+++ src/store/index.vuex.ts
@@ -201,7 +201,7 @@ export class Store extends VuexModule {
               ) -
                 eventTimeOnDay(d); */
 
-            /* try {
+            try {
               getTotalTime(chunksByDay[d]);
             } catch (err) {
               alert(`chunksByDay[d]: ${chunksByDay[d]}`);
@@ -239,7 +239,7 @@ export class Store extends VuexModule {
               alert("error 4: getting event time on day " + err);
             }
 
-            dayToAssign = d; */
+            dayToAssign = d;
             /*
             if (dayHasTime) {
               anyDaysWork = true;
@@ -250,23 +250,6 @@ export class Store extends VuexModule {
             if (combinedDayData[d] <= combinedDayData[dayToAssign]) {
               dayToAssign = d;
             } */
-
-            const dayHasTime =
-              getTotalTime(chunksByDay[d]) + chunkDuration <=
-              this.settings.timeOnDay(
-                DateUtils.applyDayOffset(d, DateUtils.currentDate)
-              ) -
-                eventTimeOnDay(d);
-
-            if (dayHasTime) {
-              anyDaysWork = true;
-            } else {
-              continue; // If the time is greater than what we can spend on this day, try the next one
-            }
-
-            if (combinedDayData[d] <= combinedDayData[dayToAssign]) {
-              dayToAssign = d;
-            }
           }
         }
 
